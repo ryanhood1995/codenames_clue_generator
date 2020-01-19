70e4a7c6152e3c1d683404c3f7d23410a6b61e53
Ryan Hood <ryanchristopherhood@gmail.com>
2020-01-19 12:25:23 -0600

    Shortened main considerably

--- main.py
+++ main.py
@@ -6,10 +6,7 @@
 # -------------------------------------------------------------------------------
 
 import gensim
-import get_result_set
-import json
-import get_scores
-import get_final_suggestions
+import play_game
 
 # ----------------------------
 # -----------MAIN-------------
@@ -20,33 +17,5 @@ model = gensim.models.KeyedVectors.load_word2vec_format(
     'GoogleNews-vectors-negative300.bin.gz', binary=True, limit=500000
 )
 
-# Input the board.
-board = {
-    'blue': [
-        'berry', 'opera', 'day', 'plastic',
-        'undertaker', 'tower', 'fan', 'stream'
-    ],
-    'red': [
-        'kangaroo', 'stock', 'centaur', 'web',
-        'crane', 'europe', 'jack', 'engine', 'bottle'
-    ],
-    'assassin': 'helicopter'
-}
-
-# Get the "result set" which is a set of words that are most related to the board words.
-result_set = get_result_set.get_both_result_set(model, board)
-
-# Convert the result set into scores for each word in the result set.
-blue_scores = get_scores.get_scores(model, both_result_sets['blue'], board['blue'])
-red_scores = get_scores.get_scores(model, both_result_sets['red'], board['red'])
-
-# Use the scores to get the final suggestions.
-blue_suggestions = get_final_suggestions.scores_to_complete_suggestions(blue_scores)
-red_suggestions = get_final_suggestions.scores_to_complete_suggestions(red_scores)
-
-# Display those suggestions.
-print("BLUE SUGGESTIONS:")
-print(blue_suggestions)
-
-print("RED SUGGESTIONS")
-print(red_suggestions)
+# Now it is time to choose the 'game-mode'.  The options are in the play_game.py file.
+play_game.get_quick_suggestions(model)
